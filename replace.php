3ca960410148e518db3281ecdc04f9bdfb7cf399
Ben Le <goben2489@gmail.com>
2022-12-20 11:45:03 +0700

    Update some case replace not error when use $1 $2

--- replace.php
+++ replace.php
@@ -1,4 +1,4 @@
 <?php
 //Example replace a content between <h1>(.*?)</h1>
-preg_replace('/(<h1.*?>).*?(<\/h1>)/', '$1'.$seoContent.'$2', $orgContent)
+preg_replace('/<h1[^>]*>([\s\S]*?)<\/h1[^>]*>/', '<h1>'.$seoContent.'</h1>', $orgContent);
 ?>
